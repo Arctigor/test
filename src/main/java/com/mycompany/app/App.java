8025bd0c2f960e0ad45d315b486914a3bc3e65ae
Szabolcs Bene <bene_szabolcs@yahoo.com>
2015-03-05 00:02:23 +0200

    Init Weld with logger.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,27 @@
 package com.mycompany.app;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Hello JEnkins!!!" );
-        System.out.println( "Hello JEnkins!!asdfasdf!" );
-    }
+@ApplicationScoped
+public class App {
+	private static final XLogger LOG = XLoggerFactory.getXLogger(App.class);
+
+	public App() {
+
+	}
+
+	public void printHello(@Observes ContainerInitialized event) {
+
+		LOG.info("Hello Weld");
+
+	}
 }
