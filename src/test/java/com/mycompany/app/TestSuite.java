622b228b5e8adec1aafd18b6d83406460625ae52
Szabolcs Bene <bene_szabolcs@yahoo.com>
2015-03-07 15:04:49 +0200

    some comm.

---
+++ src/test/java/com/mycompany/app/TestSuite.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	AppTest.class,
+	Class1Test.class
+})
+public final class TestSuite {
+	
+}
