02ef4ebf055fc81f0f46b28754e9aad3666d4ecd
Szabolcs Bene <bene_szabolcs@yahoo.com>
2015-03-07 13:39:18 +0200

    test not working

--- src/main/java/com/mycompany/app/Class1Test.java
+++ src/main/java/com/mycompany/app/Class1Test.java
@@ -18,5 +18,11 @@ public class Class1Test {
 		
 		//fail("Not yet implemented");
 	}
+	@Test
+	public void testGetName2() {
+		assertEquals(true, false);
+		
+		//fail("Not yet implemented");
+	}
 
 }
